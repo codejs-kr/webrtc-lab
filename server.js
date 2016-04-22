c272e3739142f1eaec4598884f8158c467ff9ad7
dodortus <dodortus@hotmail.com>
2016-04-22 18:07:26 +0900

    * add capture

--- server.js
+++ server.js
@@ -16,13 +16,17 @@ app.get('/intro', function(req, res) {
 	res.render('intro.html', {
     title: "- WebRTC 소개"
   });
+}).get('/getUserMedia', function(req, res) {
+	res.render('getUserMedia.html', {
+    title: "- 마이크 & 캠 접근하기"
+  });
 }).get('/filter', function(req, res) {
 	res.render('filter.html', {
     title: "- 비디오에 필터 적용하기"
   });
-}).get('/getUserMedia', function(req, res) {
-	res.render('getUserMedia.html', {
-    title: "- 마이크 & 캠 접근하기"
+}).get('/capture', function(req, res) {
+	res.render('capture.html', {
+    title: "- 비디오를 이미지로 캡쳐하기"
   });
 }).get('/conference', function(req, res) {
 	res.render('conference.html', {
