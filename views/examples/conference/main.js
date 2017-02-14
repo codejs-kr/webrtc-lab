b7c5c6d9152caac96182e62dd87aa4347abab93d
dodortus <dodortus@gmail.com>
2017-02-14 14:16:38 +0900

    * modify video codec to h264

--- views/examples/conference/main.js
+++ views/examples/conference/main.js
@@ -97,6 +97,8 @@ $(function() {
 
     peer.addStream(localStream); // addStream 제외시 recvonly로 SDP 생성됨
     peer.createOffer(function(SDP) {
+      SDP.sdp = SDP.sdp.replace("100 101 107", "107 100 101"); // h264 first
+
       peer.setLocalDescription(SDP);
       console.log("Sending offer description", SDP);
       send({
