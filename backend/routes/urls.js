b203b06a94d38b7f9afb1228411b9d2d6d0cc5c3
dodortus <dodortus@gmail.com>
2021-05-01 22:13:16 +0900

    refactor: 라우터 수정

---
+++ backend/routes/urls.js
@@ -0,0 +1,57 @@
+module.exports = [
+  {
+    title: null,
+    url: '/',
+    renderFile: 'index.ejs',
+  },
+  {
+    title: 'WebRTC 소개',
+    url: '/intro',
+    renderFile: 'intro.ejs',
+  },
+  {
+    title: '마이크 & 캠 접근하기',
+    url: '/get-user-media',
+    renderFile: 'examples/get-user-media/index.ejs',
+  },
+  {
+    title: '비디오에 필터 적용하기',
+    url: '/filter',
+    renderFile: 'examples/filter/index.ejs',
+  },
+  {
+    title: '비디오를 이미지로 캡쳐하기',
+    url: '/capture',
+    renderFile: 'examples/capture/index.ejs',
+  },
+  {
+    title: '1:1 화상회의 만들기',
+    url: '/conference',
+    renderFile: 'examples/conference/index.ejs',
+  },
+  {
+    title: '화면 공유',
+    url: '/screen-share',
+    renderFile: 'examples/screen-share/index.ejs',
+  },
+  {
+    title: '파일 & 데이터 전송하기',
+    url: '/data-channel',
+    renderFile: 'examples/data-channel/index.ejs',
+  },
+  {
+    title: '음성 인식',
+    url: '/speech-recognition',
+    renderFile: 'examples/speech-recognition/index.ejs',
+  },
+  {
+    title: '멀티 스트림',
+    url: '/multi-stream',
+    renderFile: 'examples/multi-stream/index.ejs',
+  },
+  {
+    title: '다이나믹 레졸루션',
+    url: '/dynamic-resolution',
+    renderFile: 'examples/dynamic-resolution/index.ejs',
+  },
+];
