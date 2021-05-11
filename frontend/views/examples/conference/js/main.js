dfb30a71c342468be1fb360759376156878a500e
dodortus <dodortus@gmail.com>
2021-05-11 23:23:23 +0900

    refactor: nativejs

--- frontend/views/examples/conference/js/main.js
+++ frontend/views/examples/conference/js/main.js
@@ -7,239 +7,254 @@ import MediaHandler from './modules/media-handler.js';
  * @author dodortus (dodortus@gmail.com / codejs.co.kr)
  *
  */
-$(function () {
-  console.log('Loaded Main');
-
-  const socket = io();
-  const mediaHandler = new MediaHandler();
-  const peerHandler = new PeerHandler({ send });
-  const animationTime = 500;
-  const isSafari = DetectRTC.browser.isSafari;
-  const isMobile = DetectRTC.isMobileDevice;
-  const mediaConstraints = {
-    audio: true,
-    video: {
-      width: {
-        ideal: 1280,
-        min: 640,
-        max: 1920,
-      },
-      height: {
-        ideal: 720,
-        min: 360,
-        max: 1080,
-      },
-      frameRate: {
-        ideal: 25,
-      },
-      // Select the front/user facing camera or the rear/environment facing camera if available (on Phone)
-      facingMode: 'user',
+const socket = io();
+const mediaHandler = new MediaHandler();
+const peerHandler = new PeerHandler({ send });
+const animationTime = 500;
+const isSafari = DetectRTC.browser.isSafari;
+const isMobile = DetectRTC.isMobileDevice;
+const mediaConstraints = {
+  audio: true,
+  video: {
+    width: {
+      ideal: 1280,
+      min: 640,
+      max: 1920,
     },
-  };
+    height: {
+      ideal: 720,
+      min: 360,
+      max: 1080,
+    },
+    frameRate: {
+      ideal: 25,
+    },
+    // Select the front/user facing camera or the rear/environment facing camera if available (on Phone)
+    facingMode: 'user',
+  },
+};
 
-  let roomId;
-  let userId;
-  let remoteUserId;
-  let isOffer;
+let roomId;
+let userId;
+let remoteUserId;
+let isOffer;
 
-  // DOM
-  const $body = $('body');
-  const $createWrap = $('#create-wrap');
-  const $waitWrap = $('#wait-wrap');
-  const $videoWrap = $('#video-wrap');
-  const $uniqueToken = $('#unique-token');
+// DOM
+const $body = document.body;
+const $createWrap = document.querySelector('#create-wrap');
+const $waitWrap = document.querySelector('#wait-wrap');
+const $videoWrap = document.querySelector('#video-wrap');
+const $uniqueToken = document.querySelector('#unique-token');
 
-  async function getUserMedia() {
-    const stream = await peerHandler.getUserMedia(mediaConstraints, isOffer);
-    onLocalStream(stream);
+async function getUserMedia() {
+  const stream = await peerHandler.getUserMedia(mediaConstraints, isOffer);
+  onLocalStream(stream);
+}
+
+/**
+ * 입장 후 다른 참여자 발견시 호출
+ */
+function onDetectUser() {
+  console.log('onDetectUser');
+
+  $waitWrap.innerHTML = `
+    <div class="room-info">
+      <p>당신을 기다리고 있어요. 참여 하실래요?</p>
+      <button id="btn-join">Join</button>
+    </div>
+  `;
+
+  document.querySelector('#btn-join').addEventListener('click', (e) => {
+    e.target.disabled = true;
+    isOffer = true;
+    getUserMedia();
+  });
+
+  $createWrap.classList.add('slideup');
+}
+
+/**
+ * 참석자 핸들링
+ * @param roomId
+ * @param userList
+ */
+function onJoin(roomId, userList) {
+  console.log('onJoin', roomId, userList);
+
+  if (Object.size(userList) > 1) {
+    onDetectUser();
+  }
+}
+
+/**
+ * 이탈자 핸들링
+ * @param userId
+ */
+function onLeave(userId) {
+  console.log('onLeave', arguments);
+
+  if (remoteUserId === userId) {
+    document.querySelector('#remote-video').remove();
+    $body.classList.remove('connected');
+    $body.classList.add('wait');
+    remoteUserId = null;
+  }
+}
+
+/**
+ * 소켓 메세지 핸들링
+ * @param data
+ */
+function onMessage(data) {
+  console.log('onMessage', arguments);
+
+  if (!remoteUserId) {
+    remoteUserId = data.sender;
   }
 
-  /**
-   * 입장 후 다른 참여자 발견시 호출
-   */
-  function onDetectUser() {
-    console.log('onDetectUser');
-
-    $waitWrap.html(`
-        <div class="room-info">
-          <p>당신을 기다리고 있어요. 참여 하실래요?</p>
-          <button id="btn-join">Join</button>
-        </div>
-      `);
-
-    $('#btn-join').click(function () {
-      $(this).attr('disabled', true);
-      isOffer = true;
-      getUserMedia();
-    });
-
-    $createWrap.slideUp(animationTime);
+  if (data.sdp || data.candidate) {
+    peerHandler.signaling(data);
+  } else {
+    // etc
   }
+}
 
-  /**
-   * 참석자 핸들링
-   * @param roomId
-   * @param userList
-   */
-  function onJoin(roomId, userList) {
-    console.log('onJoin', roomId, userList);
+/**
+ * 소켓 메시지 전송
+ * @param data
+ */
+function send(data) {
+  console.log('send', arguments);
 
-    if (Object.size(userList) > 1) {
-      onDetectUser();
-    }
+  data.roomId = roomId;
+  data.sender = userId;
+  socket.send(data);
+}
+
+/**
+ * 방 고유 접속 토큰 생성
+ */
+function setRoomToken() {
+  const hashValue = (Math.random() * new Date().getTime()).toString(32).toUpperCase().replace(/\./g, '-');
+
+  if (location.hash.length > 2) {
+    $uniqueToken.href = location.href;
+  } else {
+    location.hash = '#' + hashValue;
   }
+}
 
-  /**
-   * 이탈자 핸들링
-   * @param userId
-   */
-  function onLeave(userId) {
-    console.log('onLeave', arguments);
+/**
+ * 클립보드 복사
+ */
+function setClipboard() {
+  $uniqueToken.addEventListener('click', () => {
+    const link = location.href;
 
-    if (remoteUserId === userId) {
-      $('#remote-video').remove();
-      $body.removeClass('connected').addClass('wait');
-      remoteUserId = null;
-    }
-  }
-
-  /**
-   * 소켓 메세지 핸들링
-   * @param data
-   */
-  function onMessage(data) {
-    console.log('onMessage', arguments);
-
-    if (!remoteUserId) {
-      remoteUserId = data.sender;
-    }
-
-    if (data.sdp || data.candidate) {
-      peerHandler.signaling(data);
+    if (window.clipboardData) {
+      window.clipboardData.setData('text', link);
+      alert('Copy to Clipboard successful.');
     } else {
-      // etc
+      window.prompt('Copy to clipboard: Ctrl+C, Enter', link); // Copy to clipboard: Ctrl+C, Enter
     }
+  });
+}
+
+function createVideoEl(id) {
+  const $video = document.createElement('video');
+  $video.id = id || 'new-video';
+  $video.muted = true;
+  $video.autoplay = true;
+
+  return $video;
+}
+
+/**
+ * 로컬 스트림 핸들링
+ * @param stream
+ */
+function onLocalStream(stream) {
+  console.log('onLocalStream', stream);
+
+  const $localVideo = createVideoEl('local-video');
+  $videoWrap.insertBefore($localVideo, $videoWrap.childNodes[0]);
+
+  mediaHandler.setVideoStream({
+    type: 'local',
+    el: $localVideo,
+    stream: stream,
+  });
+
+  $body.className = 'room wait';
+
+  if (isMobile && isSafari) {
+    mediaHandler.playForIOS($localVideo);
   }
+}
 
-  /**
-   * 소켓 메시지 전송
-   * @param data
-   */
-  function send(data) {
-    console.log('send', arguments);
+/**
+ * 상대방 스트림 핸들링
+ * @param stream
+ */
+function onRemoteStream(stream) {
+  console.log('onRemoteStream', stream);
 
-    data.roomId = roomId;
-    data.sender = userId;
-    socket.send(data);
+  const $remoteVideo = createVideoEl('remote-video');
+  $videoWrap.insertBefore($remoteVideo, $videoWrap.childNodes[0]);
+
+  mediaHandler.setVideoStream({
+    type: 'remote',
+    el: $remoteVideo,
+    stream: stream,
+  });
+
+  $body.classList.remove('wait');
+  $body.classList.add('connected');
+
+  if (isMobile && isSafari) {
+    mediaHandler.playForIOS($remoteVideo);
   }
+}
 
-  /**
-   * 방 고유 접속 토큰 생성
-   */
-  function setRoomToken() {
-    const hashValue = (Math.random() * new Date().getTime()).toString(32).toUpperCase().replace(/\./g, '-');
+/**
+ * DOM 이벤트 바인딩
+ */
+function bindDomEvent() {
+  document.querySelector('#btn-start').addEventListener('click', getUserMedia);
+  document.querySelector('#btn-camera').addEventListener('click', (e) => {
+    const $this = e.target;
+    $this.classList.toggle('active');
+    mediaHandler[$this.className === 'active' ? 'pauseVideo' : 'resumeVideo']();
+  });
+  document.querySelector('#btn-mic').addEventListener('click', (e) => {
+    const $this = e.target;
+    $this.classList.toggle('active');
+    mediaHandler[$this.className === 'active' ? 'muteAudio' : 'unmuteAudio']();
+  });
 
-    if (location.hash.length > 2) {
-      $uniqueToken.attr('href', location.href);
-    } else {
-      location.hash = '#' + hashValue;
-    }
-  }
+  document.querySelector('#btn-change-resolution')?.addEventListener('click', peerHandler.changeResolution);
+}
 
-  /**
-   * 클립보드 복사
-   */
-  function setClipboard() {
-    $uniqueToken.click(function () {
-      const link = location.href;
+/**
+ * 초기 설정
+ */
+function initialize() {
+  setRoomToken();
+  setClipboard();
+  roomId = location.href.replace(/\/|:|#|%|\.|\[|\]/g, '');
+  userId = Math.round(Math.random() * 99999);
 
-      if (window.clipboardData) {
-        window.clipboardData.setData('text', link);
-        alert('Copy to Clipboard successful.');
-      } else {
-        window.prompt('Copy to clipboard: Ctrl+C, Enter', link); // Copy to clipboard: Ctrl+C, Enter
-      }
-    });
-  }
+  // 소켓 관련 이벤트 바인딩
+  socket.emit('enter', roomId, userId);
+  socket.on('join', onJoin);
+  socket.on('leave', onLeave);
+  socket.on('message', onMessage);
 
-  /**
-   * 로컬 스트림 핸들링
-   * @param stream
-   */
-  function onLocalStream(stream) {
-    $videoWrap.prepend('<video id="local-video" muted="muted" autoplay />');
-    const localVideo = document.querySelector('#local-video');
-    mediaHandler.setVideoStream({
-      type: 'local',
-      el: localVideo,
-      stream: stream,
-    });
+  // Peer 관련 이벤트 바인딩
+  peerHandler.on('addRemoteStream', onRemoteStream);
 
-    $body.addClass('room wait');
+  // DOM 이벤트 바인딩
+  bindDomEvent();
+}
 
-    if (isMobile && isSafari) {
-      mediaHandler.playForIOS(localVideo);
-    }
-  }
-
-  /**
-   * 상대방 스트림 핸들링
-   * @param stream
-   */
-  function onRemoteStream(stream) {
-    console.log('onRemoteStream', stream);
-
-    $videoWrap.prepend('<video id="remote-video" autoplay />');
-    const remoteVideo = document.querySelector('#remote-video');
-    mediaHandler.setVideoStream({
-      type: 'remote',
-      el: remoteVideo,
-      stream: stream,
-    });
-
-    $body.removeClass('wait').addClass('connected');
-
-    if (isMobile && isSafari) {
-      mediaHandler.playForIOS(remoteVideo);
-    }
-  }
-
-  /**
-   * 초기 설정
-   */
-  function initialize() {
-    setRoomToken();
-    setClipboard();
-    roomId = location.href.replace(/\/|:|#|%|\.|\[|\]/g, '');
-    userId = Math.round(Math.random() * 99999);
-
-    // 소켓 관련 이벤트 바인딩
-    socket.emit('enter', roomId, userId);
-    socket.on('join', onJoin);
-    socket.on('leave', onLeave);
-    socket.on('message', onMessage);
-
-    // Peer 관련 이벤트 바인딩
-    peerHandler.on('addRemoteStream', onRemoteStream);
-
-    $('#btn-start').click(getUserMedia);
-
-    $('#btn-camera').click(function () {
-      const $this = $(this);
-      $this.toggleClass('active');
-      mediaHandler[$this.hasClass('active') ? 'pauseVideo' : 'resumeVideo']();
-    });
-
-    $('#btn-mic').click(function () {
-      const $this = $(this);
-      $this.toggleClass('active');
-      mediaHandler[$this.hasClass('active') ? 'muteAudio' : 'unmuteAudio']();
-    });
-
-    $('#btn-change-resolution').click(function () {
-      peerHandler.changeResolution();
-    });
-  }
-
-  initialize();
-});
+initialize();
